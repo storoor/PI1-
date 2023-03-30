ae47d45b3a6137e40ee04ce190360eb3538332d2
Santiago <storoor@eafit.edu.co>
2023-03-30 11:43:47 -0500

    Solución taller parte 1

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models  import Movie
 
 # Create your views here.
 def home(request):
     #return render(request,'home.html')
-    return render(request,'home.html', {'name':'Santiago Toro Orozco'})
+    #return render(request,'home.html', {'name':'Santiago Toro Orozco'})
+    searchTerm =request.GET.get('searchMovie')
+    if searchTerm:
+        movies=Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies=Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
+
+def about(request):
+    return render (request,'about.html')
